cc0450aabd028a29938f4035f100a99ce7bd5186
Matthew Douglass <5410142+mdouglass@users.noreply.github.com>
2020-07-29 23:37:44 -0700

    wip

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require('@actions/core')
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const now = new Date()
+    const context = {
+      year: now.getUTCFullYear(),
+      month: now.getUTCMonth() + 1,
+      date: now.getUTCDate(),
+      sha: 
+    }
+    let contextProgram = ''
+    for (const [key, value] of Object.entries(context)) {
+      if (typeof key === 'object' && key !== null) {
+        contextProgram += `var ${key} = JSON.parse('${JSON.stringify(value)}');\n`
+      } else {
+        contextProgram += `var ${key} = ${value};\n`
+      }
+    }
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const formatString = core.getInput('format').replace('`', '\\`')
+    const templateProgram = '`' + formatString + '`'
 
-    core.setOutput('time', new Date().toTimeString());
+    const version = eval(contextProgram + templateProgram)
+    core.setOutput('version', version)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
