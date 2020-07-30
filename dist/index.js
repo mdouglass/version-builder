f034b9d47abd34b8d32b6b9fb23601d291ffecb4
Matthew Douglass <5410142+mdouglass@users.noreply.github.com>
2020-07-30 09:04:46 -0700

    Add git context

--- dist/index.js
+++ dist/index.js
@@ -4458,6 +4458,16 @@ function padZero(value, maxLength = 2) {
   return String(value).padStart(maxLength, '0')
 }
 
+function __getPRNumber() {
+  const match = /refs\/pull\/(\d+)\/merge/.exec(githubModule.context.ref)
+  return match ? match.groups[1] : undefined
+}
+
+function __getPR() {
+  const pr = __getPRNumber()
+  return pr ? `PR-${pr}` : undefined
+}
+
 module.exports.format = function format(formatString) {
   const now = new Date()
   const year = now.getUTCFullYear()
@@ -4467,7 +4477,14 @@ module.exports.format = function format(formatString) {
   const minutes = now.getUTCMinutes()
   const seconds = now.getUTCSeconds()
   const github = githubModule.context
+  const git = {
+    prNumber: __getPRNumber(),
+    pr: __getPR(),
+    branch: 'unknown',
+  }
   const env = process.env
+  coreModule.debug('git: ' + JSON.stringify(git))
+  coreModule.debug('github: ' + JSON.stringify(github))
   return eval('`' + formatString.replace('`', '\\`') + '`')
 }
 
